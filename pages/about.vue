6ce561377175d44e14172d66a576e52af4451365
Kevin Wang <b11032004@mail.ntust.edu.tw>
2025-12-18 23:57:59 +0800

    [Frontend] Update about

--- pages/about.vue
+++ pages/about.vue
@@ -2,12 +2,22 @@
   <div>
     <VCard>
       <VCardTitle>
-        <span class="text-3xl">玖玖巴音效版</span>
+        <span class="text-3xl">梅奧奧音效版</span>
       </VCardTitle>
       <VCardText class="text-lg">
         <ul class="list-disc pl-6">
           <li>
-            製作:
+            作者:
+            <a
+              class="link"
+              href="https://twitter.com/OUO34993952"
+              target="_blank"
+            >
+              OUO
+            </a>
+          </li>
+          <li>
+            模板提供:
             <a class="link" href="https://konnokai.me/" target="_blank">
               孤之界
             </a>
@@ -24,8 +34,8 @@
               九条夏目
             </a>
           </li>
-          <li>音檔素材來源: Twitch Clip、玖玖巴 Discord 音效板、茄子阿光</li>
-          <li>圖檔素材來源: 玖玖巴 Discord</li>
+          <li>音檔素材來源: Twitch Clip、Akatsuki、斷骨</li>
+          <li>圖檔素材來源:</li>
         </ul>
       </VCardText>
     </VCard>
